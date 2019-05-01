2b47e6bde12870c6c83639e72ee2a94a0d42ff58
Marta <43724620+gulidamarta@users.noreply.github.com>
2019-05-01 16:25:43 +0300

    Update model.js

--- src/app/model.js
+++ src/app/model.js
@@ -2,93 +2,57 @@ import View from './view';
 
 export default class Model{
     constructor(){  
-        this.lastAPIUrl = '';
-        this.pageToShow = 0;
         this.newsDisplayed = 0;
         this.view = new View();
     }
 
-    getNewsResources(){
-        let newsAPIUrl = 'https://newsapi.org/v2/sources?apiKey=9cbcc49e8fdd445684f8d622f33125b4';
-        let request = new Request(newsAPIUrl);
-
-        fetch(request)
-          .then((response) => { 
-              return response.json(); 
-            })
-          .then((data) => {
-            for (let i = 0; i < data.sources.length; i++) {
-              document.querySelector('#resources').innerHTML +=
-               '<button class="btn btn__sources" id="' + data.sources[i].id + '">' 
-               + data.sources[i].name + '</button>';
-            }      
-          });
+    processNewsResources(data){
+      for (let i = 0; i < data.sources.length; i++) {
+        document.querySelector('#resources').innerHTML +=
+         '<button class="btn btn__sources" id="' + data.sources[i].id + '">' 
+         + data.sources[i].name + '</button>';
+      } 
     }
 
-    getNewsBySearch(urlPart){
-        document.querySelector('#error-block').style.display = 'none';
-        let newsAPIUrl = 'https://newsapi.org/v2/' + urlPart + 'apiKey=9cbcc49e8fdd445684f8d622f33125b4';
-        let request = new Request(newsAPIUrl);
+    processNewsBySearch(data){
+      const newsCount = data.articles.length;
+      const newsItems = document.querySelector('#news');
+      newsItems.innerHTML = '';
+     
+      if(newsCount == 0){
+          document.querySelector('#error-block').style.display = 'unset';  
+          document.querySelector('#loadButton').style.display = 'none';
+          return;
+      }      
 
-        let currThis = this;
-        fetch(request)
-          .then((response) => { 
-                return response.json(); 
-            })
-          .then((data) => {
+      const renderedItems = this.view.renderItems(newsCount, data.articles);
+      newsItems.appendChild(renderedItems);
 
-            const newsCount = data.articles.length;
-            const newsItems = document.querySelector('#news');
-            newsItems.innerHTML = '';
-           
-            if(newsCount == 0){
-                document.querySelector('#error-block').style.display = 'unset';  
-                document.querySelector('#loadButton').style.display = 'none';
-                return;
-            }      
+      const minDisplayedNews = 5;
 
-            const renderedItems = currThis.view.renderItems(newsCount, data.articles);
-            newsItems.appendChild(renderedItems);
+      if(newsCount < minDisplayedNews)
+          document.querySelector('#loadButton').style.display = 'none';
+      else
+          document.querySelector('#loadButton').style.display = 'unset';
+      this.newsDisplayed = newsCount;  
+    }
 
-            const minDisplayedNews = 5;
-            if(newsCount < minDisplayedNews)
-                document.querySelector('#loadButton').style.display = 'none';
-            else
-                document.querySelector('#loadButton').style.display = 'unset';
-            
-            currThis.lastAPIUrl = newsAPIUrl;
-            currThis.pageToShow = 2;
-            currThis.newsDisplayed = newsCount;
-          });
-      }
 
-    appendNews(){
-        this.lastAPIUrl = this.lastAPIUrl.replace(new RegExp('page=.*&'), 'page=' + this.pageToShow + '&');
-        let request = new Request(this.lastAPIUrl);
+    processAppendNews(data){
+      const newsCount = data.articles.length;
+      if(newsCount == 0){
+          document.querySelector('#loadButton').style.display = 'none';
+          return;
+      }    
 
-        var currThis = this;
-        fetch(request)
-          .then((response) => { 
-              return response.json(); 
-            })
-          .then((data) => {
-            const newsCount = data.articles.length;
-            if(newsCount == 0){
-                document.querySelector('#loadButton').style.display = 'none';
-                return;
-            }    
+      const renderedItems = this.view.renderItems(newsCount, data.articles);
+      const newsItems = document.querySelector('#news');
+      newsItems.appendChild(renderedItems);
 
-            const renderedItems = currThis.view.renderItems(newsCount, data.articles);
-            const newsItems = document.querySelector('#news');
-            newsItems.appendChild(renderedItems);
+      this.newsDisplayed += newsCount;
+      const minDisplayedNews = 5, maxDisplayedNews = 40;
 
-            currThis.newsDisplayed += newsCount;
-            currThis.pageToShow++;
-            const minDisplayedNews = 5, maxDisplayedNews = 40;
-
-            if(newsCount < minDisplayedNews || newsDisplayed == maxDisplayedNews)
-               document.querySelector('#loadButton').style.display = 'none';
-          });
-      }
-
+      if(newsCount < minDisplayedNews || this.newsDisplayed == maxDisplayedNews)
+         document.querySelector('#loadButton').style.display = 'none';
+    }
 }
