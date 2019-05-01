436f9bac2ac540c42e5a0d476e456f07df8f2d03
Marta <43724620+gulidamarta@users.noreply.github.com>
2019-05-01 16:25:02 +0300

    Update controller.js

--- src/app/controller.js
+++ src/app/controller.js
@@ -3,24 +3,72 @@ import Model from './model';
 export default class Controller{
     constructor(){
         this.model = new Model();
+        this.pageToShow = 0;
+        this.lastAPIUrl = '';
+    }
+
+    getNewsResources(){
+        let newsAPIUrl = 'https://newsapi.org/v2/sources?apiKey=9cbcc49e8fdd445684f8d622f33125b4';
+        let request = new Request(newsAPIUrl);
+
+        let currThis = this;
+        fetch(request)
+          .then((response) => { 
+              return response.json(); 
+            })
+          .then((data) => {
+              currThis.model.processNewsResources(data);     
+          });
+    }
+
+    getNewsBySearch(urlPart){
+        document.querySelector('#error-block').style.display = 'none';
+        let newsAPIUrl = 'https://newsapi.org/v2/' + urlPart + 'apiKey=9cbcc49e8fdd445684f8d622f33125b4';
+        let request = new Request(newsAPIUrl);
+
+        let currThis = this;
+        fetch(request)
+          .then((response) => { 
+                return response.json(); 
+            })
+          .then((data) => {
+              currThis.model.processNewsBySearch(data);
+              currThis.pageToShow = 2;
+              currThis.lastAPIUrl = newsAPIUrl;
+          });
+    }
+
+    appendNews(){
+        this.lastAPIUrl = this.lastAPIUrl.replace(new RegExp('page=.*&'), 'page=' + this.pageToShow + '&');
+        let request = new Request(this.lastAPIUrl);
+
+        var currThis = this;
+        fetch(request)
+          .then((response) => { 
+              return response.json(); 
+            })
+          .then((data) => {
+              currThis.model.processAppendNews(data);
+              currThis.pageToShow++;
+          });
     }
 
     start(){
-        this.model.getNewsResources();
-        this.model.getNewsBySearch('top-headlines?country=us&pageSize=5&page=1&');
+        this.getNewsResources();
+        this.getNewsBySearch('top-headlines?country=us&pageSize=5&page=1&');
 
         document.querySelector('#resources').addEventListener('click', (event) =>{
-            this.model.getNewsBySearch(`everything?sources=${event.target.id}&pageSize=5&page=1&`);
+            this.getNewsBySearch(`everything?sources=${event.target.id}&pageSize=5&page=1&`);
           });
 
         document.querySelector('#loadButton').addEventListener('click', () => {
-            this.model.appendNews();   
+            this.appendNews();   
         });
 
         document.querySelector('#search-btn').addEventListener('click', () => {
             const query = document.querySelector('#search-field').value;
             if(query.length > 0){
-                this.model.getNewsBySearch(`everything?q=${query}&pageSize=5&page=1&`);
+                this.getNewsBySearch(`everything?q=${query}&pageSize=5&page=1&`);
                 query.length = 0;
             }
         });
